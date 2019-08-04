4db1c36422bd031c4bafecc220ec361377311e3e
Asven Asven <edwardgreatmen@mail.ru>
2019-08-04 10:23:11 +1000

    Test commit

---
+++ Delegate_traning/Charapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_training
+{
+    interface Charapter
+    {
+        string Name { get; }
+        int HP { get; set; }
+        int Damage { get; set; }
+        int Experience { get; set; }
+        byte Level { get; set; }
+        int LevelBoundary { get; set; }
+
+        byte LevelUp(byte onelvl);
+        int ExperienceUp();
+        int HPUp();
+        int HDDown();
+        int Attack();
+    }
+}
---
+++ Delegate_traning/Hero.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_training
+{
+    class Hero : Charapter
+    {
+        public string Name { get; }
+        public int HP { get ; set ; }
+        public int Damage { get ; set ; }
+        public int Experience { get ; set ; }
+        public byte Level { get ; set ; }
+        public int LevelBoundary { get; set; } = 100;
+
+        public Hero(string name, int dmg, int hp, int exp, byte lvl)
+        {
+            Name = name;
+            Damage = dmg;
+            HP = hp;
+            Experience = exp;
+            Level = lvl;
+        }
+
+        public byte LevelUp(byte onelvl)
+        {
+            if (Experience >= LevelBoundary & levelUpHandler != null)
+            {
+                Experience -= LevelBoundary;
+                return levelUpHandler(Level++);
+            }
+            else return Level;///
+        }
+
+        public int ExperienceUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int HPUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int HDDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Attack()
+        {
+            throw new NotImplementedException();
+        }
+
+        public delegate byte LevelUpHandler(byte onelvl);
+        private LevelUpHandler levelUpHandler;
+        public void RegisterWithLevelUp(LevelUpHandler methodToCall)
+        { levelUpHandler = methodToCall; }
+    }
+}
